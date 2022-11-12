1075951bb35d908ad8601ce09de7341666e7c549
AikaNiyazova <aikaniyazova@gmail.com>
2022-11-12 17:55:12 +0600

    fixed

--- src/main/java/lang/print/gaps/task4/FormulaCreator.java
+++ src/main/java/lang/print/gaps/task4/FormulaCreator.java
@@ -3,10 +3,10 @@ package lang.print.gaps.task4;
 public class FormulaCreator {
     public static void main(String[] args) {
 
-        char a = 'a';
-        char b = 'b';
+        int a;
+        int b;
 
-        System.out.printf("(9 * (%c ^ 2) - 5 * %c + 2 + %c - 7) * ((%c + %c - 4 * %c * %c) / 2)", a, b, a,
-                a, b, a, b);
+        System.out.println("(9 * (a ^ 2) - 5 * b + 2 + a - 7) * ((a + b - 4 * a * b) / 2)");
+
     }
 }
